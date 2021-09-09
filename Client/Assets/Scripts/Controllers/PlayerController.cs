c1d0826988492f03d09b477e68603b9f4840f377
newmri <newmri@naver.com>
2021-09-09 23:14:41 +0900

    플레이어 이동 추가

---
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        Vector3 pos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetDirInput();
+        UpdatePosition();
+        UpdateIsMoving();
+    }
+
+    void GetDirInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Dir = MoveDir.Up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Dir = MoveDir.Down;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Dir = MoveDir.Left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Dir = MoveDir.Right;
+        }
+        else
+        {
+            Dir = MoveDir.None;
+        }
+    }
+
+    void UpdatePosition()
+    {
+        if (!_isMoving)
+            return;
+
+        Vector3 destPos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 moveDir = destPos - transform.position;
+
+        float dist = moveDir.magnitude;
+        if (dist < _speed * Time.deltaTime)
+        {
+            transform.position = destPos;
+            _isMoving = false;
+        }
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            _isMoving = true;
+        }
+    }
+
+    void UpdateIsMoving()
+    {
+        if (!_isMoving)
+        {
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    _cellPos += Vector3Int.up;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Down:
+                    _cellPos += Vector3Int.down;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Left:
+                    _cellPos += Vector3Int.left;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Right:
+                    _cellPos += Vector3Int.right;
+                    _isMoving = true;
+                    break;
+            }
+        }
+    }
+
+    MoveDir _dir = MoveDir.Down;
+
+    public MoveDir Dir
+    {
+        get { return _dir; }
+        set
+        {
+            if (value == _dir)
+                return;
+
+            switch (value)
+            {
+                case MoveDir.Up:
+                    _animator.Play("WALK_BACK");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("WALK_FRONT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.None:
+                    if (_dir == MoveDir.Up)
+                    {
+                        _animator.Play("IDLE_BACK");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    else if (_dir == MoveDir.Down)
+                    {
+                        _animator.Play("IDLE_FRONT");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    else if (_dir == MoveDir.Left)
+                    {
+                        _animator.Play("IDLE_RIGHT");
+                        transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    }
+                    else
+                    {
+                        _animator.Play("IDLE_RIGHT");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    break;
+            }
+
+            _dir = value;
+        }
+    }
+
+    public float _speed = 5.0f;
+    
+    bool _isMoving = false;
+    Vector3Int _cellPos = Vector3Int.zero;
+    public Grid _grid;
+
+    Animator _animator;
+
+}
